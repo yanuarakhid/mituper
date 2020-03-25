7d9380e56c931f46d7c715671b52f908e1ac1ebb
yanuarakhid <yanuarakhid@gmail.com>
2020-03-25 23:29:29 +0700

    Add  static IP Address Configuration

--- core/core.py
+++ core/core.py
@@ -12,13 +12,14 @@ def menu():
         print(
             '''
             ------------ CONFIGURATION MENU -----------
-            
+
             [1] Check Internet Connection (ping google.com)
-            [2] Ping to other 
-            [3] Reboot The Router
-            [4] Shutdown The Router
-            [5] System Resource
-            [6] Exit Program
+            [2] Ping to other
+            [3] Set IP Address
+            [4] Reboot The Router
+            [5] Shutdown The Router
+            [6] System Resource
+            [7] Exit Program
 
             -------------------------------------------
             '''
@@ -34,16 +35,37 @@ def menu():
             pingOther(ping)
             menu = input() or "menu"
         elif choice == 3:
+            os.system("clear")
+            print('''
+By Default We Set
+
+IP Address  : 10.100.88.1
+Prefix      : /24
+Interface   : ether2
+Comment     : MY LAN NETWORK
+
+You Can Set To Other IP Address, Prefix, Interface, or Comment
+            ''')
+            input("Enter to Show Interface/IP Address and Continue")
+            time.sleep(1)
+            show_interface()
+            set_ip_address = input("Set IP Address : ") or "10.100.88.1"
+            set_prefix = input("Set Prefix : ") or "24"
+            set_interface = input("Input your interface : ") or "ether2"
+            set_comment = input("Input your interface : ") or "MY LAN NETWORK"
+            set_ip(set_ip_address, set_prefix, set_interface, set_comment)
+            menu = input() or "menu"
+        elif choice == 4:
             ping = input("Ping To : ")
             pingOther(ping)
             menu = input() or "menu"
-        elif choice == 4:
+        elif choice == 5:
             shutdown()
             exit()
-        elif choice == 5:
+        elif choice == 6:
             resource()
             menu = input() or "menu"
-        elif choice == 6:
+        elif choice == 7:
             exit()
         else:
             print("Wrong Choice, Please Choice 1, 2, 3")
@@ -68,7 +90,7 @@ def login():
     ''')
     global host, username, passwd, port
     host = input(
-        "Router Address (Ex:example.com/192.168.88.1) : ") or "10.100.69.4"
+        "Router Address (Ex:example.com/192.168.88.1) : ") or "10.100.69.17"
     username = input("Username  : ") or "admin"
     passwd = getpass("Password: ") or "qwe321"
     port = input("SSH Port : ") or 22
@@ -138,3 +160,33 @@ def resource():
     output = stdout.readlines()
     print("\n".join(output))
     ssh_connect.close()
+
+
+def show_interface():
+    global host, username, passwd, port
+    ssh_connect = paramiko.SSHClient()
+    ssh_connect.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh_connect.connect(hostname=host, username=username,
+                        password=passwd, port=port, timeout=5)
+    stdin, stdout, stderr = ssh_connect.exec_command('ip address print')
+    time.sleep(1)
+
+    output = stdout.readlines()
+    print("\n".join(output))
+    ssh_connect.close()
+
+
+def set_ip(ip, prefix, interface, comment):
+    global host, username, passwd, port
+    ssh_connect = paramiko.SSHClient()
+    ssh_connect.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh_connect.connect(hostname=host, username=username,
+                        password=passwd, port=port, timeout=5)
+    stdin, stdout, stderr = ssh_connect.exec_command(
+        'ip address add address='+ip+'/'+prefix+' interface='+interface+' comment="'+comment+'"')
+    time.sleep(1)
+
+    output = stdout.readlines()
+    print("\n".join(output))
+    ssh_connect.close()
+    show_interface()
